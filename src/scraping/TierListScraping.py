d8d67c813eef906b515d0a1f784dce9706f8fef1
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2023-11-27 09:39:48 -0300

    feat: create TierListScraping class

---
+++ src/scraping/TierListScraping.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+class TierListScraping:
+    def __init__(self, link: str) -> None:
+        self._GAMEPRESS_LINK = link
+
+        page = requests.get(self._GAMEPRESS_LINK)
+        html_content = page.text
+        self._soup = BeautifulSoup(html_content, "html.parser")
