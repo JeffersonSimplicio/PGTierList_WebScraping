96545aa902eb976355ede69f94c6f1489b9d41ef
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2023-11-27 11:24:07 -0300

    feat: Create PokeRankingJSON class

---
+++ src/PokeRankingJSON.py
@@ -0,0 +1,15 @@
+import json
+from src.utilities.Terminal import Terminal
+from src.PokemonModel import Pokémon
+from src.scraping.PokeInfoScraping import PokeInfoScraping
+from src.scraping.TierListScraping import TierListScraping
+
+
+class PokeRankingJSON:
+    def __init__(self, tier_list_link) -> None:
+        self.tier_list_link = tier_list_link
+
+        self.GAMEPRESS_LINK = "https://gamepress.gg"
+        self.tier_list = TierListScraping(
+            self.GAMEPRESS_LINK+self.tier_list_link
+        )
