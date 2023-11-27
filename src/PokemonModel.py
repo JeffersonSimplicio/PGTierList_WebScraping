2412d70a2f96c9873db9fd6d05bb82a904054fd9
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2023-11-27 10:27:48 -0300

    feat: add method to check if Pokemon is Shadow

--- src/PokemonModel.py
+++ src/PokemonModel.py
@@ -11,3 +11,17 @@ class Pokémon:
         self.quick_attack = quick_attack
         self.charged_attack = charged_attack
         self.api_name = self.name.lower()
+        # common cases
+        self._is_shadow = "shadow" in self.api_name
+
+    def _poke_api_generate(self) -> str:
+        # common cases
+        self._if_case(self._is_shadow, "shadow", "")
+
+        return f"{self._API_POKE}/{self.api_name}"
+
+    def _if_case(self, condition: bool, old: str, new: str) -> None:
+        if condition:
+            self.api_name = self.api_name\
+                .replace(old, new)\
+                .strip()
