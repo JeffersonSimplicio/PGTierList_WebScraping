24ba5d6e9887d474d26c38ad12b850ea92e191b7
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2023-11-27 11:28:32 -0300

    feat: add method to generate a dictionary with Pokemon ranking

--- src/PokeRankingJSON.py
+++ src/PokeRankingJSON.py
@@ -16,3 +16,59 @@ class PokeRankingJSON:
 
     def number_pokemon(self):
         return self.tier_list.number_pokemon()
+
+    def generate_ranking(self) -> dict[str, dict[str, list[dict[str, any]]]]:
+        poke_ranking = dict()
+        real_tier_list = self.tier_list.run()
+        number_pokemon = self.tier_list.number_pokemon()
+        count = 0
+
+        for key, value in real_tier_list.items():
+            rank = "_".join(key.split()).lower()
+            for poke in value:
+                count += 1
+                poke_name = poke["name"]
+                poke_link = poke["link"]
+                # Terminal
+                Terminal.clear()
+                print(f"{count}/{number_pokemon}")
+                print(f"{((count / number_pokemon) * 100):.2f}%")
+                print(f"Pokemon atual: {poke_name}")
+
+                info_poke = PokeInfoScraping(self.GAMEPRESS_LINK+poke_link)
+                datas = info_poke.run()
+
+                poke_info: dict[str, any]
+                if len(datas) != 0:
+                    for type_, attacks in datas.items():
+                        quick_attack = []
+                        charged_attack = []
+                        for attack in attacks:
+                            quick_attack.append(attack[0])
+                            charged_attack.append(attack[1])
+                        x = Pokémon(
+                            poke_name,
+                            type_,
+                            quick_attack,
+                            charged_attack
+                        )
+                        poke_info = x.generate()
+                else:
+                    poke_type = info_poke.get_poke_types()
+                    x = Pokémon(
+                        poke_name,
+                        poke_type,
+                        [],
+                        []
+                    )
+                    poke_info = x.generate()
+
+                if type_ in poke_ranking:
+                    if rank in poke_ranking[type_]:
+                        poke_ranking[type_][rank].append(poke_info)
+                    else:
+                        poke_ranking[type_][rank] = [poke_info]
+                else:
+                    poke_ranking[type_] = {rank: [poke_info]}
+
+        return poke_ranking
