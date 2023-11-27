64ef1956b4fdcd13bb2873d247c6f2c093361efa
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2023-11-27 11:37:07 -0300

    feat: Add method to generate a hash

--- src/Security.py
+++ src/Security.py
@@ -2,3 +2,10 @@ import bcrypt
 
 
 class SecurityManager:
+    @staticmethod
+    def generate_hash(password: str) -> bytes:
+        hashed = bcrypt.hashpw(
+            password.encode('utf-8'),
+            bcrypt.gensalt()
+        )
+        return hashed
