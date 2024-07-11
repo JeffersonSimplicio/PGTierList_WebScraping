5f04ae9a48dbfe917b603c06992171eaac28e643
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2024-07-11 10:09:03 -0300

    feat: Add pokemon_by_ranking method to TierListScraping class

--- src/scraping/tier_list_scraping.py
+++ src/scraping/tier_list_scraping.py
@@ -17,17 +17,41 @@ class TierListScraping:
             class_="PokemonCard_pokemonCardContent___wx3G"
         )
         return len(list_pokemon)
-    
+
     def get_name_tiers(self) -> list[str]:
         h1_elements = self._soup.select(
             "article.Card_stickyTitle__1CATW h1.Card_cardTitle__URr_A"
         )
         list_names_tiers = [h1.text for h1 in h1_elements]
         return list_names_tiers
-    
+
     def get_tier_ranking(self):
         groups = self._soup.find_all(
             "ul",
             class_="best-attackers_grid__WYqUF"
         )
         return groups
+
+    def pokemon_by_ranking(self) -> dict[str, list[dict[str,str]]]:
+            dict_ranking = {}
+            list_name_tier = self.get_name_tiers()
+            list_ranking_tier = self.get_tier_ranking()
+            for index, tier_name in enumerate(list_name_tier):
+                tmp = []
+                ranking_tier = list_ranking_tier[index].find_all(
+                    "li",
+                    class_="best-attackers_gridItem__thuKE"
+                )
+                for poke_cell in ranking_tier:
+                    link = poke_cell.find("a").get('href')
+                    poke_name = poke_cell.find(
+                        "span",
+                        class_="PokemonCard_pokemonCardContent___wx3G"
+                    ).text
+                    poke_data = {
+                        "name": poke_name,
+                        "link": link
+                    }
+                    tmp.append(poke_data)
+                dict_ranking.update({tier_name: tmp})
+            return dict_ranking
