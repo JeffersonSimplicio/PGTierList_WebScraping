34add62020cb547519325cb33f983c877530123d
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2024-07-10 23:57:06 -0300

    feat: add is_shiny_available property

--- src/scraping/poke_info_scraping.py
+++ src/scraping/poke_info_scraping.py
@@ -8,3 +8,10 @@ class PokeInfoScraping:
         self._driver.close_drive()
         self._soup = BeautifulSoup(self._source, "html.parser")
 
+    @property
+    def is_shiny_available(self) -> bool:
+        shiny_element = self._soup.find(
+            "span",
+            class_="PokemonPageRenderers_isShinyAvailable__sygB4"
+        )
+        return shiny_element is not None
