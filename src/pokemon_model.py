da6dfbb3eb125e37cc06e692c4e9846aa9e7838c
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2024-07-11 10:20:45 -0300

    feat: Add categorize method to PokemonModel class

--- src/pokemon_model.py
+++ src/pokemon_model.py
@@ -11,5 +11,29 @@ class PokemonModel:
         self.is_shiny_available = is_shiny_available
         self.attacks = attacks
         self.api_name = self.name.lower()
+        self.categorize()
         self._API_POKE_FORM = "pokemon-form"
         self._API_POKE = "pokemon"
+
+    def categorize(self):
+        # common cases
+        self._is_mega = "mega" in self.api_name
+        self._is_primal = "primal" in self.api_name
+        self._is_shadow = "shadow" in self.api_name
+        self._is_alola = "alola" in self.api_name
+        self._is_galar = "galar" in self.api_name
+        self._is_hisui = "hisui" in self.api_name
+        self._is_forme = "form" in self.api_name
+        self._is_x_or_y = (
+            len(self.name.split()) == 3 and
+            self.name.split()[2] in "xy"
+        )
+        # specific cases
+        self._is_genesect = "genesect" in self.api_name
+        self._is_zacian = "zacian" in self.api_name
+        self._is_hoopa = "hoopa" in self.api_name
+        self._is_darmanitan = "darmanitan" in self.api_name
+        self._is_tapu = "tapu" in self.api_name
+        # other
+        self._is_mega_or_primal = self._is_mega or self._is_primal
+        self._is_shadow = str("shadow" in self.name.lower()).lower()
