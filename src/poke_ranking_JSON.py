15149f80cab910485b7cd0fc9ba51d2b8c7ed30e
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2024-07-11 11:21:00 -0300

    feat: Add generate method to PokeRankingJSON class

--- src/poke_ranking_JSON.py
+++ src/poke_ranking_JSON.py
@@ -11,3 +11,29 @@ class PokeRankingJSON:
         self.TierList = TierListScraping(LINK_BASE+LINK_TIER)
         self.TierListDict = self.TierList.pokemon_by_ranking()
 
+    def generate(self) -> dict[str[list[dict[str,any]]]]:
+        counter = 0
+        total_pokemons = self.TierList.count_pokemon()
+        percentage = (counter/total_pokemons)*100
+        poke_tier_ranking = {}
+        for tier, poke_list in self.TierListDict.items():
+            print(f'Tier: {tier}')
+            tmp_tier_list = []
+            for pokemon in poke_list:
+                name = pokemon["name"]
+                link = pokemon["link"]
+                counter+= 1
+                percentage = (counter/total_pokemons)*100
+                print(f'Pokemon atual: {name}')
+                print(f'{counter} de {total_pokemons}, {percentage:.2f}%')
+                print(f'Resta: {total_pokemons-counter}')
+                poke_info_base = PokeInfoScraping(LINK_BASE+link)
+                poke_data = PokemonModel(
+                    name,
+                    poke_info_base.get_typing(),
+                    poke_info_base.isShinyAvailable(),
+                    poke_info_base.get_attacks()
+                )
+                tmp_tier_list.append(poke_data.generate())
+            poke_tier_ranking[tier] = tmp_tier_list
+        return poke_tier_ranking
