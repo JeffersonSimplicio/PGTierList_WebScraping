0532b0a47986ea9576ac382984962d9932343ea8
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2024-07-10 23:31:58 -0300

    feat: add get_page_source method

--- src/scraping/web_scraper.py
+++ src/scraping/web_scraper.py
@@ -5,3 +5,6 @@ class WebScraper:
         self.driver = webdriver.Edge()
         self.driver.get(url)
         self.source = self.driver.page_source
+
+    def get_page_source(self) ->str:
+        return self.source
