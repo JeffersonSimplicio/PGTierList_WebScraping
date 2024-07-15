f4bc14a6e4df98386d25ff571b6efc85c8da0217
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2024-07-15 12:23:42 -0300

    feat: add read method to JsonHandler class

--- src/utilities/json_Handler.py
+++ src/utilities/json_Handler.py
@@ -3,4 +3,16 @@ import json
 
 class JsonHandler:
     def __init__(self, file_path: str) -> None:
-        self.file_path = file_path  
+        self.file_path = file_path
+
+    def read(self):
+        try:
+            with open(self.file_path, 'r') as file:
+                data = json.load(file)
+            return data
+        except FileNotFoundError:
+            print(f"Arquivo {self.file_path} não encontrado.")
+            return None
+        except json.JSONDecodeError:
+            print("Erro ao decodificar o JSON.")
+            return None
