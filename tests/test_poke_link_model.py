4735b22ec809a7147a3836ffd23ca9795a4b35e0
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2024-10-02 12:31:08 -0300

    test: add unit test for __repr__ method in PokeLinkModel class

--- tests/test_poke_link_model.py
+++ tests/test_poke_link_model.py
@@ -15,9 +15,18 @@ def test_poke_lonk_initialization(poke_link):
     assert poke_link.link == LINK
 
 
-def test_poke_attack_to_dict(poke_link):
+def test_poke_link_to_dict(poke_link):
     expected_dict = {
         "name": NAME,
         "link": LINK
     }
     assert poke_link.to_dict() == expected_dict
+
+
+def test_poke_link_repr(poke_link):
+    expected_repr = (
+        "PokeLinkModel(name='Pikachu', "
+        "link='www.thisisatest.com/pikachu')"
+    )
+
+    assert repr(poke_link) == expected_repr
