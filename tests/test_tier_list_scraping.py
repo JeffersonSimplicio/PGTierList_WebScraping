1838cb06b32e748bce3237af75aae36d8e9602f4
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2024-10-02 13:29:00 -0300

    test: create test file for TierListScraping class and set up initial configurations

---
+++ tests/test_tier_list_scraping.py
@@ -0,0 +1,33 @@
+import pytest
+from unittest.mock import patch, MagicMock
+from bs4.element import Tag
+from src.scraping.tier_list_scraping import TierListScraping
+
+
+# Mockando a classe WebScraper
+@pytest.fixture
+def mock_web_scraper():
+    with patch(
+        "src.scraping.tier_list_scraping.WebScraper"
+    ) as MockWebScraper:
+        mock_driver = MockWebScraper.return_value
+        mock_driver.setup_soup.return_value = MagicMock()
+        yield MockWebScraper
+
+
+# Mockando a classe PokeLinkModel
+@pytest.fixture
+def mock_poke_link_model():
+    with patch(
+        "src.scraping.tier_list_scraping.PokeLinkModel"
+    ) as MockPokeLinkModel:
+        yield MockPokeLinkModel
+
+
+# Fixture para o scraper
+@pytest.fixture
+def scraper(mock_web_scraper, mock_poke_link_model):
+    return TierListScraping(
+        mock_poke_link_model,
+        mock_web_scraper.return_value
+    )
