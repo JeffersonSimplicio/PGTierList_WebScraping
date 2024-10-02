7762cb84742f3a78c580cda2cf941608d6ae3357
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2024-10-02 12:26:36 -0300

    refactor: create constants in tests to avoid code repetition

--- tests/test_poke_attack_model.py
+++ tests/test_poke_attack_model.py
@@ -1,27 +1,31 @@
 import pytest
 from src.models.poke_attack_model import PokeAttackModel
 
+TYPE_ATTACK = "Electric"
+FAST_ATTACK = "Thunder Shock"
+CHARGED_ATTACK = "Thunderbolt"
+
 
 @pytest.fixture
 def poke_attack():
     return PokeAttackModel(
-        type_attack="Electric",
-        fast_attack="Thunder Shock",
-        charged_attack="Thunderbolt",
+        type_attack=TYPE_ATTACK,
+        fast_attack=FAST_ATTACK,
+        charged_attack=CHARGED_ATTACK,
     )
 
 
 def test_poke_attack_initialization(poke_attack):
-    assert poke_attack.type == "Electric"
-    assert poke_attack.fast_attack == "Thunder Shock"
-    assert poke_attack.charged_attack == "Thunderbolt"
+    assert poke_attack.type == TYPE_ATTACK
+    assert poke_attack.fast_attack == FAST_ATTACK
+    assert poke_attack.charged_attack == CHARGED_ATTACK
 
 
 def test_poke_attack_to_dict(poke_attack):
     expected_dict = {
-        "type": "Electric",
-        "fast_attack": "Thunder Shock",
-        "charged_attack": "Thunderbolt",
+        "type": TYPE_ATTACK,
+        "fast_attack": FAST_ATTACK,
+        "charged_attack": CHARGED_ATTACK,
     }
     assert poke_attack.to_dict() == expected_dict
 
