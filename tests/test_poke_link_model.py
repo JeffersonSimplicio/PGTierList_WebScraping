62aeadbeb90a242489d213db34a9926e8a684bab
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2024-10-02 12:21:35 -0300

    test: add unit test for to_dict method in PokeLinkModel class

--- tests/test_poke_link_model.py
+++ tests/test_poke_link_model.py
@@ -1,15 +1,23 @@
 import pytest
 from src.models.poke_link_model import PokeLinkModel
 
+NAME = "Pikachu"
+LINK = "www.thisisatest.com/pikachu"
+
 
 @pytest.fixture
-def poke_attack():
-    return PokeLinkModel(
-        name="Pikachu",
-        link="www.thisisatest.com/pikachu"
-    )
+def poke_link():
+    return PokeLinkModel(name=NAME, link=LINK)
 
 
-def test_poke_attack_initialization(poke_attack):
-    assert poke_attack.name == "Pikachu"
-    assert poke_attack.link == "www.thisisatest.com/pikachu"
+def test_poke_lonk_initialization(poke_link):
+    assert poke_link.name == NAME
+    assert poke_link.link == LINK
+
+
+def test_poke_attack_to_dict(poke_link):
+    expected_dict = {
+        "name": NAME,
+        "link": LINK
+    }
+    assert poke_link.to_dict() == expected_dict
