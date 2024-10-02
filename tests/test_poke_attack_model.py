2b8c77bdb197cb8f4e71a9ac155f0586e32c3af9
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2024-10-02 12:01:02 -0300

    test: add unit test for to_dict method in PokeAttackModel class

--- tests/test_poke_attack_model.py
+++ tests/test_poke_attack_model.py
@@ -15,3 +15,12 @@ def test_poke_attack_initialization(poke_attack):
     assert poke_attack.type == "Electric"
     assert poke_attack.fast_attack == "Thunder Shock"
     assert poke_attack.charged_attack == "Thunderbolt"
+
+
+def test_poke_attack_to_dict(poke_attack):
+    expected_dict = {
+        "type": "Electric",
+        "fast_attack": "Thunder Shock",
+        "charged_attack": "Thunderbolt",
+    }
+    assert poke_attack.to_dict() == expected_dict
