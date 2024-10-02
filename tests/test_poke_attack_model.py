70130df70f3182283d2ccd2b6a053400b0cded03
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2024-10-02 12:02:17 -0300

    test: add unit test for __repr__ method in PokeAttackModel class

--- tests/test_poke_attack_model.py
+++ tests/test_poke_attack_model.py
@@ -24,3 +24,13 @@ def test_poke_attack_to_dict(poke_attack):
         "charged_attack": "Thunderbolt",
     }
     assert poke_attack.to_dict() == expected_dict
+
+
+def test_poke_attack_repr(poke_attack):
+    expected_repr = (
+        "PokemonAttack(type=Electric, "
+        "fast_attack=Thunder Shock, "
+        "charged_attack=Thunderbolt)"
+    )
+
+    assert repr(poke_attack) == expected_repr
