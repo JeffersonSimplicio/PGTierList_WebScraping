10ff40d5dd9c3970572ae46886cecad2fc4e5b41
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2024-10-09 12:04:46 -0300

    feat: add LinkChecker class to verify the functionality of generated links

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from src.models.poke_attack_model import PokeAttackModel
 from src.scraping.web_scraper import WebScraper
 from src.scraping.poke_info_scraping import PokeInfoScraping
 from src.scraping.tier_list_scraping import TierListScraping
+from src.utilities.link_checker import LinkChecker
+
 
 LINK_BASE = "https://db.pokemongohub.net"
 LINK_TIER = "/best/raid-attackers"
@@ -17,5 +19,7 @@ ranking = PokeRankingJSON(
     LINK_BASE,
     LINK_TIER
 )
-
 ranking.generate_json()
+
+checker = LinkChecker()
+checker.save_broken_links()
---
+++ src/utilities/link_checker.py
@@ -0,0 +1,40 @@
+from requests import get
+from src.utilities.json_Handler import JsonHandler
+from src.utilities.terminal import Terminal
+
+
+class LinkChecker:
+
+    def __init__(self, url_file: str = "pg_tier_list.json"):
+        self.url_data = JsonHandler(url_file).read()
+        self.broken_links = []
+
+    def check_links(self):
+        for tier, poke_list in self.url_data.items():
+            print(f"Tier atual: {tier}")
+            for poke_info in poke_list:
+                poke_name = poke_info["name"]
+                poke_link = poke_info["api_name"]
+                print(f"Pokemon em analise: {poke_name}")
+                print(f"Link do Pokemon em analise: {poke_link}")
+
+                response = get(poke_link)
+                if response.status_code != 200:
+                    self.broken_links.append(f"{poke_name} - {poke_link}")
+                    Terminal.error(f"O Pokemon {poke_name}, tá com problema.")
+                print("-- " * 50)
+            print("## " * 50)
+
+    def save_broken_links(self, file_name: str = "broken_links.txt"):
+        self.check_links()
+
+        with open(file_name, "w") as f:
+            for link in self.broken_links:
+                f.write(link + "\n")
+        Terminal.success(f"Links problemáticos salvos em: {file_name}")
+
+
+# Exemplo de uso
+if __name__ == "__main__":
+    checker = LinkChecker()
+    checker.save_broken_links()
