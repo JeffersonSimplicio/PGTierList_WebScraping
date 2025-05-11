2014837d8d1400f1895409b13a84e20920ae62d3
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2025-05-11 14:51:16 -0300

    refactor: fix return type in to_dict and improve documentation of AbstractModel class

--- src/models/abstract_model.py
+++ src/models/abstract_model.py
@@ -1,9 +1,13 @@
 from abc import ABC, abstractmethod
+from typing import Any
 
 
 class AbstractModel(ABC):
+    """Base interface to enforce consistent model
+    behavior across the application."""
+
     @abstractmethod
-    def to_dict(self) -> dict[str, any]:
+    def to_dict(self) -> dict[str, Any]:
         """Converts the model to a dictionary."""
         pass
 
