37296019c11bc0a71fe363a3078fe28ead1988fc
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2025-05-11 15:05:05 -0300

    refactoring: encapsulate attributes of the PokeLinkModel class using properties

--- src/models/poke_link_model.py
+++ src/models/poke_link_model.py
@@ -3,17 +3,25 @@ from src.models.abstract_model import AbstractModel
 
 class PokeLinkModel(AbstractModel):
     def __init__(self, name: str, link: str):
-        self.name = name
-        self.link = link
+        self._name = name
+        self._link = link
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def link(self) -> str:
+        return self._link
 
     def to_dict(self) -> dict[str, str]:
-        return {
-            "name": self.name,
-            "link": self.link
-        }
+        return {"name": self._name, "link": self._link}
 
     def __repr__(self):
-        return f"PokeLinkModel(name='{self.name}', link='{self.link}')"
+        return (
+            f"{self.__class__.__name__}(name='{self._name}', "
+            f"link='{self._link}')"
+        )
 
     def __str__(self) -> str:
-        return f"Pokémon name: {self.name},\nLink to details: {self.link}"
+        return f"Pokémon name: {self._name},\nLink to details: {self._link}"
