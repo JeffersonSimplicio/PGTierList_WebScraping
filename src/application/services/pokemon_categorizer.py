275177255349ee2509a426f3a173315ce469ae01
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2025-05-13 18:54:35 -0300

    refactor: PokemonCategorizer to use keyword categories and add new keyword_categories constant

--- src/application/services/pokemon_categorizer.py
+++ src/application/services/pokemon_categorizer.py
@@ -6,42 +6,30 @@ from src.domain.services.poke_name_sanitizer import PokeNameSanitizer
 class PokemonCategorizer(Classifier):
     def __init__(
         self,
+        keyword_categories: list[str],
         name_sanitizer: PokeNameSanitizer | None = None
     ) -> None:
+        self.keyword_categories = keyword_categories
         self._name_sanitizer = name_sanitizer
 
-    def classify(self, pokemon_name: str):
+    def classify(self, pokemon_name: str) -> dict[str, bool]:
         self._pokemon_name = self._sanitize_name(pokemon_name)
 
         categories = {
-            # Common Cases
-            "is_mega": self._has("mega"),
-            "is_primal": self._has("primal"),
-            "is_shadow": self._has("shadow"),
-            "is_forme": self._has("form"),
-            "is_x_or_y": self._is_x_or_y(),
-            # Specific Cases
-            "is_genesect": self._has("genesect"),
-            "is_zacian": self._has("zacian"),
-            "is_hoopa": self._has("hoopa"),
-            "is_darmanitan": self._has("darmanitan"),
-            "is_tapu": self._has("tapu"),
-            "is_necrozma_form": self._is_necrozma_form(),
-            "is_deoxys": self._has("deoxys"),
-            "is_keldeo": self._has("keldeo"),
-            "is_zamazenta": self._has("zamazenta"),
-            "is_mr_rime": self._has("rime"),
-            "is_porygon": self._has("porygon"),
-            "is_hooh": self._has("hooh"),
-            # Regions
-            "is_alola": self._has("alola"),
-            "is_galar": self._has("galar"),
-            "is_hisui": self._has("hisui"),
+            f"is_{term}": self._has(term)
+            for term in self.keyword_categories
         }
 
-        categories["is_mega_or_primal"] = (
-            categories["is_mega"] or categories["is_primal"]
+        categories.update(
+            {
+                "is_x_or_y": self._is_x_or_y(),
+                "is_necrozma_form": self._is_necrozma_form(),
+                "is_mega_or_primal": (
+                    categories["is_mega"] or categories["is_primal"]
+                ),
+            }
         )
+
         return categories
 
     def _sanitize_name(self, pokemon_name: str) -> str:
@@ -52,10 +40,10 @@ class PokemonCategorizer(Classifier):
     def _has(self, term: str) -> bool:
         return term in self._pokemon_name
 
-    def _is_x_or_y(self):
+    def _is_x_or_y(self) -> bool:
         return len(self._pokemon_name.split()) == 3 and bool(
             search(r"\bx\b|\by\b", self._pokemon_name, IGNORECASE)
         )
 
-    def _is_necrozma_form(self):
+    def _is_necrozma_form(self) -> bool:
         return self._has("necrozma") and len(self._pokemon_name.split()) > 1
---
+++ src/domain/constants/keyword_categories.py
@@ -0,0 +1,21 @@
+common_keyword_categories = ["mega", "primal", "shadow", "form"]
+specific_keyword_categories = [
+    "genesect",
+    "zacian",
+    "hoopa",
+    "darmanitan",
+    "tapu",
+    "deoxys",
+    "keldeo",
+    "zamazenta",
+    "rime",
+    "porygon",
+    "hooh",
+]
+region_keyword_categories = ["alola", "galar", "hisui"]
+
+all_keyword_categories = (
+    common_keyword_categories
+    + specific_keyword_categories
+    + region_keyword_categories
+)
