a5cba8df4ee91311b33de95f4eca8a019e201533
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2025-05-13 13:07:41 -0300

    feat: add PokeAttack class and update type of attacks in Pokemon

---
+++ src/domain/entities/poke_attack.py
@@ -0,0 +1,5 @@
+class PokeAttack:
+    def __init__(self, attack_type: str, fast: str, charge: str):
+        self.attack_type = attack_type
+        self.fast = fast
+        self.charged = charge
--- src/domain/entities/pokemon.py
+++ src/domain/entities/pokemon.py
@@ -1,10 +1,13 @@
+from src.domain.entities.poke_attack import PokeAttack
+
+
 class Pokemon:
     def __init__(
         self,
         id: int,
         name: str,
         types: list[str],
-        attacks: list[str],
+        attacks: list[PokeAttack],
         is_shiny_available: bool,
         categories: dict[str, bool],
         poke_api: str,
